8f864777f4aba64f1763f6ec736df01ca5e2539d
Hannes Aronsson <aronssonhannes@gmail.com>
2017-02-27 21:57:44 +0100

    Imported the code for the random classes.

--- src/barber/random/ExponentialRandomStream.java
+++ src/barber/random/ExponentialRandomStream.java
@@ -1,7 +1,26 @@
 package barber.random;
 
+import java.util.Random;
+
 /**
  * Created by Mumrik on 2017-02-27.
  */
 public class ExponentialRandomStream {
+
+    private Random rand;
+    private double lambda;
+
+    public ExponentialRandomStream(double lambda, long seed) {
+        rand = new Random(seed);
+        this.lambda = lambda;
+    }
+
+    public ExponentialRandomStream(double lambda) {
+        rand = new Random();
+        this.lambda = lambda;
+    }
+
+    public double next() {
+        return -Math.log(rand.nextDouble())/lambda;
+    }
 }
