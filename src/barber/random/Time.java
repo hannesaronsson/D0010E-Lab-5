cea296c228a425ce50d2b167ba453eb2f81e89b9
Hannes Aronsson <aronssonhannes@gmail.com>
2017-02-27 23:09:07 +0100

    Bugfix in Time.java

--- src/barber/random/Time.java
+++ src/barber/random/Time.java
@@ -16,33 +16,29 @@ public class Time {
     private double totalIdleTime;
     private double totalQueueTime;
 
+
     /**
      * Constructor
      *
-     * @param lambda
-     *      Constant that indicates how many costumers that enters every hour on average.
-     * @param seed
-     *      Seed for random generator.
-     * @param lowerHairCut
-     *      The lower bound of the time it takes for a haircut.
-     * @param upperHairCut
-     *      The upper bound of the time it takes for a haircut.
-     * @param lowerDissatisfied
-     *      The lower bound of the time it takes for a dissatisfied customer to return.
-     * @param upperDissatisfied
-     *      The upper bound of the time it takes for a dissatisfied customer to return.
+     * @param lambda            Constant that indicates how many costumers that enters every hour on average.
+     * @param seed              Seed for random generator.
+     * @param lowerHairCut      The lower bound of the time it takes for a haircut.
+     * @param upperHairCut      The upper bound of the time it takes for a haircut.
+     * @param lowerDissatisfied The lower bound of the time it takes for a dissatisfied customer to return.
+     * @param upperDissatisfied The upper bound of the time it takes for a dissatisfied customer to return.
      */
     public Time(double lambda, long seed, double lowerHairCut, double upperHairCut,
                 double lowerDissatisfied, double upperDissatisfied) {
         this.ERS = new ExponentialRandomStream(lambda, seed);
         this.URSHaircut = new UniformRandomStream(lowerHairCut, upperHairCut);
         this.URSDissatisfied = new UniformRandomStream(lowerDissatisfied, upperDissatisfied);
+        timeList.add((double) 0);
     }
 
     /**
      * Gives the next arrival time for an arrival event.
-     * @return
-     *       The next time for an arrival event as a double.
+     *
+     * @return The next time for an arrival event as a double.
      */
     public double nextArrivalTime() {
         double timeToNextEvent = ERS.next();
@@ -54,8 +50,8 @@ public class Time {
 
     /**
      * Gives the total idle time of the simulation.
-     * @return
-     *      Total idle time.
+     *
+     * @return Total idle time.
      */
     public double getTotalIdleTime() {
         return totalIdleTime;
@@ -63,8 +59,8 @@ public class Time {
 
     /**
      * Gives the total queue time for the simulation.
-     * @return
-     *      Total queue time.
+     *
+     * @return Total queue time.
      */
     public double getTotalQueueTime() {
         return totalQueueTime;
@@ -72,8 +68,8 @@ public class Time {
 
     /**
      * Gives the next hair cut time for a hair cut event.
-     * @return
-     *      The next time for a hair cut.
+     *
+     * @return The next time for a hair cut.
      */
     public double nextHairCutTime() {
         double timeToNextEvent = URSHaircut.next();
@@ -85,8 +81,8 @@ public class Time {
 
     /**
      * Gives the next time for an returning customer that was dissatisfied for a dissatisfied event.
-     * @return
-     *      The next time for a dissatisfied customer arrival.
+     *
+     * @return The next time for a dissatisfied customer arrival.
      */
     public double nextReturnedDissatisfied() {
         double timeToNextEvent = URSDissatisfied.next();
@@ -97,8 +93,6 @@ public class Time {
     }
 
     //TODO: QueueTime
-
-
 
 
 }
